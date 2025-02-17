5ceb006b5d0b59e6f36477aa662a468fd8136d60
ronald <ronald@DESKTOP-I8AS5D4.>
2025-02-17 15:51:01 -0400

    Fix: homebrew script

--- runs/01-homebrew.zsh
+++ runs/01-homebrew.zsh
@@ -6,11 +6,26 @@ if ! command -v brew &>/dev/null; then
 
     # Cargar Homebrew en la sesión actual
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    
+    # Descargar configuraciones .zshrc y .zprofile
+    echo "Descargando configuraciones .zshrc y .zprofile..."
+    curl -fsSL https://github.com/Ronaldcdz/dotfiles/blob/main/zsh/.zshrc -o "$HOME/.zshrc"
+    curl -fsSL https://github.com/Ronaldcdz/dotfiles/blob/main/zsh/.zprofile -o "$HOME/.zprofile"
+
+    # Esperar un segundo para asegurar que el entorno se haya cargado
+    sleep 1
+
+    # Verificar si brew funciona
+    if command -v brew &>/dev/null; then
+        echo "Homebrew instalado y cargado correctamente."
+    else
+        echo "Hubo un problema cargando Homebrew."
+        exit 1
+    fi
 
     # Agregar la configuración de Homebrew al .zshrc para futuras sesiones
     echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> "$HOME/.zshrc"
 
-    echo "Homebrew instalado y configurado correctamente."
 else
     echo "Homebrew ya está instalado."
 fi
