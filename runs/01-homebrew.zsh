468f1e889c0518ae409f7be34063df66f3aeb78f
ronald <ronald@DESKTOP-I8AS5D4.>
2025-02-17 16:08:54 -0400

    Fix: the real solution

--- runs/01-homebrew.zsh
+++ runs/01-homebrew.zsh
@@ -1,8 +1,10 @@
 #!/usr/bin/env zsh
 
+USER_NAME=$(whoami)  # Obtiene el nombre del usuario actual
+
 if ! command -v brew &>/dev/null; then
     echo "Instalando Homebrew..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    /bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
     # Cargar Homebrew en la sesión actual
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
@@ -11,6 +13,10 @@ if ! command -v brew &>/dev/null; then
     echo "Descargando configuraciones .zshrc y .zprofile..."
     curl -fsSL https://github.com/Ronaldcdz/dotfiles/blob/main/zsh/.zshrc -o "$HOME/.zshrc"
     curl -fsSL https://github.com/Ronaldcdz/dotfiles/blob/main/zsh/.zprofile -o "$HOME/.zprofile"
+
+    # Agregar las líneas para cargar Homebrew en futuras sesiones
+    echo >> /home/$USER_NAME/.zshrc
+    echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/$USER_NAME/.zshrc
 
     # Esperar un segundo para asegurar que el entorno se haya cargado
     sleep 1
@@ -22,9 +28,6 @@ if ! command -v brew &>/dev/null; then
         echo "Hubo un problema cargando Homebrew."
         exit 1
     fi
-
-    # Agregar la configuración de Homebrew al .zshrc para futuras sesiones
-    echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> "$HOME/.zshrc"
 
 else
     echo "Homebrew ya está instalado."
