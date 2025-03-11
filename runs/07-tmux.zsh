96a35707c8c0882089e1da7694dca457866024e4
ronald <ronald@DESKTOP-I8AS5D4.>
2025-03-11 10:26:58 -0400

    fix: tmux plugins not being installing

--- runs/07-tmux.zsh
+++ runs/07-tmux.zsh
@@ -7,8 +7,8 @@ if ! command -v tmux &> /dev/null; then
 fi
 
 # Descargar el archivo de configuración .tmux.conf desde el nuevo repositorio de GitHub
-echo "Descargando archivo de configuración de tmux..."
-curl -fsSL https://raw.githubusercontent.com/Ronaldcdz/dotfiles/main/tmux/.tmux.conf --output ~/.tmux.conf
+# echo "Descargando archivo de configuración de tmux..."
+# curl -fsSL https://raw.githubusercontent.com/Ronaldcdz/dotfiles/main/tmux/.tmux.conf --output ~/.tmux.conf
 
 # Clonar el repositorio de Tmux Plugin Manager (TPM)
 if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
@@ -19,23 +19,23 @@ else
 fi
 
 # Si tmux no está en ejecución, iniciar una nueva sesión
-if ! tmux has-session 2>/dev/null; then
-    echo "Iniciando una nueva sesión de tmux..."
-    tmux new-session -d -s setup_tmux
-else
-    echo "tmux ya está en ejecución."
-fi
+# if ! tmux has-session 2>/dev/null; then
+#     echo "Iniciando una nueva sesión de tmux..."
+#     tmux new-session -d -s setup_tmux
+# else
+#     echo "tmux ya está en ejecución."
+# fi
 
 # Recargar configuración de tmux dentro de la sesión
-echo "Recargando configuración de tmux..."
-tmux send-keys -t setup_tmux "tmux source-file ~/.tmux.conf" C-m
-
-# Instalar plugins de tmux usando TPM dentro de la sesión
-echo "Instalando plugins de tmux..."
-tmux send-keys -t setup_tmux "~/.tmux/plugins/tpm/bin/install_plugins" C-m
-
-# Adjuntar a la sesión de tmux
-tmux attach-session -t setup_tmux
-
-# Recargar la configuración del shell
-source ~/.zshrc
+# echo "Recargando configuración de tmux..."
+# tmux send-keys -t setup_tmux "tmux source-file ~/.tmux.conf" C-m
+#
+# # Instalar plugins de tmux usando TPM dentro de la sesión
+# echo "Instalando plugins de tmux..."
+# tmux send-keys -t setup_tmux "~/.tmux/plugins/tpm/bin/install_plugins" C-m
+#
+# # Adjuntar a la sesión de tmux
+# tmux attach-session -t setup_tmux
+#
+# # Recargar la configuración del shell
+# source ~/.zshrc
