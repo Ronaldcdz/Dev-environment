c7361ee8dd6237ca498eabf4cc06ba30c8a4c3bf
RonaldLug <ronald.cadiz@lugotech.com.do>
2025-11-07 16:15:25 -0400

    update: snacks integration with todo comments & trouble plugins

--- dotfiles/nvim/lua/ronald/plugins/trouble.lua
+++ dotfiles/nvim/lua/ronald/plugins/trouble.lua
@@ -5,9 +5,33 @@ return {
     focus = true,
   },
   cmd = "Trouble",
+  specs = {
+    "folke/snacks.nvim",
+    opts = function(_, opts)
+      return vim.tbl_deep_extend("force", opts or {}, {
+        picker = {
+          actions = require("trouble.sources.snacks").actions,
+          win = {
+            input = {
+              keys = {
+                ["<c-t>"] = {
+                  "trouble_open",
+                  mode = { "n", "i" },
+                },
+              },
+            },
+          },
+        },
+      })
+    end,
+  },
   keys = {
     { "<leader>xw", "<cmd>Trouble diagnostics toggle<CR>", desc = "Open trouble workspace diagnostics" },
-    { "<leader>xd", "<cmd>Trouble diagnostics toggle filter.buf=0<CR>", desc = "Open trouble document diagnostics" },
+    {
+      "<leader>xd",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<CR>",
+      desc = "Open trouble document diagnostics",
+    },
     { "<leader>xq", "<cmd>Trouble quickfix toggle<CR>", desc = "Open trouble quickfix list" },
     { "<leader>xl", "<cmd>Trouble loclist toggle<CR>", desc = "Open trouble location list" },
     { "<leader>xt", "<cmd>Trouble todo toggle<CR>", desc = "Open todos in trouble" },
