3fbd676c82d76408f0b1a26cdba322f0e450f9f5
RonaldLug <ronald.cadiz@lugotech.com.do>
2025-11-24 10:53:46 -0400

    update: obsidian config

--- dotfiles/nvim/lua/ronald/plugins/obsidian.lua
+++ dotfiles/nvim/lua/ronald/plugins/obsidian.lua
@@ -5,15 +5,11 @@ return {
   enabled = true,
   ft = "markdown",
   dependencies = {
-    -- Dependency: plenary.nvim
-    -- URL: https://github.com/nvim-lua/plenary.nvim
-    -- Description: A Lua utility library for Neovim.
     "nvim-lua/plenary.nvim",
   },
 
   config = function()
     require("obsidian").setup({
-      -- Define workspaces for Obsidian
       ui = {
         enable = false,
       },
@@ -55,70 +51,13 @@ return {
         default_tags = { "daily-notes" },
       },
       callbacks = {
-        enter_note = function(_, note)
-          vim.keymap.set("n", "<leader>ch", "<cmd>Obsidian toggle_checkbox<cr>", {
+        enter_note = function(note)
+          vim.keymap.set("n", "<leader>ch", "<cmd>Obsidian smart_action<cr>", {
             buffer = note.bufnr,
             desc = "Toggle checkbox",
           })
         end,
       },
-      -- Key mappings for Obsidian commands
-      -- mappings = {
-      --   -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
-      --   ["gf"] = {
-      --     action = function()
-      --       return require("obsidian").util.gf_passthrough()
-      --     end,
-      --     opts = { noremap = false, expr = true, buffer = true },
-      --   },
-      --   -- Toggle check-boxes.
-      --   ["<leader>ch"] = {
-      --     action = function()
-      --       return require("obsidian").util.toggle_checkbox()
-      --     end,
-      --     opts = { buffer = true },
-      --     desc = "Toggle checkbox",
-      --   },
-      --   -- Smart action depending on context, either follow link or toggle checkbox.
-      --   ["<cr>"] = {
-      --     action = function()
-      --       return require("obsidian").util.smart_action()
-      --     end,
-      --     opts = { buffer = true, expr = true },
-      --   },
-      -- },
-      -- Function to generate frontmatter for notes
-      note_frontmatter_func = function(note)
-        -- This is equivalent to the default frontmatter function.
-        local out = { id = note.id, aliases = note.aliases, tags = note.tags }
-
-        -- `note.metadata` contains any manually added fields in the frontmatter.
-        -- So here we just make sure those fields are kept in the frontmatter.
-        if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
-          for k, v in pairs(note.metadata) do
-            out[k] = v
-          end
-        end
-        return out
-      end,
-
-      -- Function to generate note IDs
-      note_id_func = function(title)
-        -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
-        -- In this case a note with the title 'My new note' will be given an ID that looks
-        -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
-        local suffix = ""
-        if title ~= nil then
-          -- If title is given, transform it into valid file name.
-          suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
-        else
-          -- If title is nil, just add 4 random uppercase letters to the suffix.
-          for _ = 1, 4 do
-            suffix = suffix .. string.char(math.random(65, 90))
-          end
-        end
-        return tostring(os.time()) .. "-" .. suffix
-      end,
 
       -- Settings for templates
       templates = {
@@ -155,8 +94,6 @@ return {
         name = "snacks.pick",
         -- name = "telescope.nvim",
       },
-      sort_by = "modified",
-      sort_reversed = true,
     })
 
     vim.keymap.set("n", "<leader>ol", "<cmd>Obsidian follow_link<cr>", { desc = "follows note under cursor" })
