5a7cf15bc649b612f247dc0e2a63848972b84188
Ronald Cadiz <ronald.cadiz@lugotech.com.do>
2026-02-05 14:54:33 -0400

    add: to use with WSL

--- dotfiles/nvim/lua/ronald/plugins/lsp/mason.lua
+++ dotfiles/nvim/lua/ronald/plugins/lsp/mason.lua
@@ -1,63 +1,63 @@
 return {
-  "williamboman/mason.nvim",
-  enabled = true,
-  -- "mason-org/mason.nvim",
-  -- version = "^1.0.0",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
-  config = function()
-    -- import mason
-    local mason = require("mason")
+	"williamboman/mason.nvim",
+	enabled = true,
+	-- "mason-org/mason.nvim",
+	-- version = "^1.0.0",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		-- import mason
+		local mason = require("mason")
 
-    -- import mason-lspconfig
-    local mason_lspconfig = require("mason-lspconfig")
+		-- import mason-lspconfig
+		local mason_lspconfig = require("mason-lspconfig")
 
-    local mason_tool_installer = require("mason-tool-installer")
+		local mason_tool_installer = require("mason-tool-installer")
 
-    -- enable mason and configure icons
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-      registries = {
-        "github:Crashdummyy/mason-registry",
-        "github:mason-org/mason-registry",
-      },
-    })
+		-- enable mason and configure icons
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+			registries = {
+				"github:Crashdummyy/mason-registry",
+				"github:mason-org/mason-registry",
+			},
+		})
 
-    mason_lspconfig.setup({
-      -- list of servers for mason to install
-      ensure_installed = {
-        "ts_ls",
-        "html",
-        "cssls",
-        "tailwindcss",
-        "lua_ls",
-        "emmet_ls",
-        "pyright",
-        "vue_ls", -- era "volar"
-        -- "roslyn", -- instalar manualmente con :MasonInstall roslyn
-        -- "csharp_ls",
-        "powershell_es",
-        "vtsls",
-      },
-    })
+		mason_lspconfig.setup({
+			-- list of servers for mason to install
+			ensure_installed = {
+				"ts_ls",
+				"html",
+				"cssls",
+				"tailwindcss",
+				"lua_ls",
+				"emmet_ls",
+				"pyright",
+				"vue_ls", -- era "volar"
+				-- "roslyn", -- instalar manualmente con :MasonInstall roslyn
+				-- "csharp_ls",
+				"powershell_es",
+				-- "vtsls",
+			},
+		})
 
-    mason_tool_installer.setup({
-      ensure_installed = {
-        "prettier", -- prettier formatter
-        "stylua", -- lua formatter
-        "isort", -- python formatter
-        "black", -- python formatter
-        "pylint",
-        "eslint_d",
-      },
-    })
-  end,
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettier", -- prettier formatter
+				"stylua", -- lua formatter
+				"isort", -- python formatter
+				"black", -- python formatter
+				"pylint",
+				"eslint_d",
+			},
+		})
+	end,
 }
--- dotfiles/zsh/.zshrc
+++ dotfiles/zsh/.zshrc
@@ -76,7 +76,8 @@ if [[ -n "$TMUX" && -s "$NVM_DIR/nvm.sh" ]]; then
   source "$NVM_DIR/nvm.sh"
 fi
 
+# Configuración de NVM para Homebrew
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
+export NVM_HOMEBREW="/home/linuxbrew/.linuxbrew/opt/nvm"
+[ -s "$NVM_HOMEBREW/nvm.sh" ] && \. "$NVM_HOMEBREW/nvm.sh"
+[ -s "$NVM_HOMEBREW/etc/bash_completion.d/nvm" ] && \. "$NVM_HOMEBREW/etc/bash_completion.d/nvm"
