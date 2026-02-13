c60762b483b4c1123cf72b8500b3884a79ac52a4
Ronald Cadiz <ronald.cadiz@lugotech.com.do>
2026-02-13 10:03:09 -0400

    add: opencode plugin

---
+++ dotfiles/nvim/lua/ronald/plugins/opencode.lua
@@ -0,0 +1,43 @@
+return {
+	"nickjvandyke/opencode.nvim",
+	dependencies = {
+		-- Recommended for `ask()` and `select()`.
+		-- Required for `snacks` provider.
+		---@module 'snacks' <- Loads `snacks.nvim` types for configuration intellisense.
+		{ "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } },
+	},
+	config = function()
+		---@type opencode.Opts
+		vim.g.opencode_opts = {
+			-- Your configuration, if any — see `lua/opencode/config.lua`, or "goto definition" on the type or field.
+		}
+
+		-- Required for `opts.events.reload`.
+		vim.o.autoread = true
+
+		-- Recommended/example keymaps.
+		vim.keymap.set({ "n", "x" }, "<leader>aa", function()
+			require("opencode").ask("@this: ", { submit = true })
+		end, { desc = "Ask opencode…" })
+		vim.keymap.set({ "n", "x" }, "<leader>as", function()
+			require("opencode").select()
+		end, { desc = "Execute opencode action…" })
+		vim.keymap.set({ "n", "t" }, "<leader>at", function()
+			require("opencode").toggle()
+		end, { desc = "Toggle opencode" })
+
+		vim.keymap.set({ "n", "x" }, "<leader>ao", function()
+			return require("opencode").operator("@this ")
+		end, { desc = "Add range to opencode", expr = true })
+		vim.keymap.set("n", "<leader>aO", function()
+			return require("opencode").operator("@this ") .. "_"
+		end, { desc = "Add line to opencode", expr = true })
+
+		vim.keymap.set("n", "<S-C-u>", function()
+			require("opencode").command("session.half.page.up")
+		end, { desc = "Scroll opencode up" })
+		vim.keymap.set("n", "<S-C-d>", function()
+			require("opencode").command("session.half.page.down")
+		end, { desc = "Scroll opencode down" })
+	end,
+}
--- dotfiles/zsh/.zshrc
+++ dotfiles/zsh/.zshrc
@@ -85,3 +85,10 @@ export NVM_DIR="$HOME/.nvm"
 export NVM_HOMEBREW="/home/linuxbrew/.linuxbrew/opt/nvm"
 [ -s "$NVM_HOMEBREW/nvm.sh" ] && \. "$NVM_HOMEBREW/nvm.sh"
 [ -s "$NVM_HOMEBREW/etc/bash_completion.d/nvm" ] && \. "$NVM_HOMEBREW/etc/bash_completion.d/nvm"
+
+# bun completions
+[ -s "/home/ronald/.bun/_bun" ] && source "/home/ronald/.bun/_bun"
+
+# bun
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
